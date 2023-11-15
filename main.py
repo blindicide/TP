7f613b72befe6b0d8eb6fc2a36677d0a207e112f
blindicide-pc <y1rs.yrs@gmail.com>
2023-11-15 11:25:40 +0300

    ///

--- main.py
+++ main.py
@@ -11,17 +11,22 @@ token = tokenfile.read()
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types=['text'])
-if message.text == '/start':
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-    data = dt_string.split(' ')[0]
-    time = dt_string.split(' ')[1]
-    if int(time.split(':')[0]) < 11:
-        var_time = "Доброе утро,"
-    elif int(time.split(':')[1]) >= 11 and int(time.split(':')[1])< 18:
-        var_time = "Добрый день,"
-    else:
-        var_time = "Добрый вечер,"
-    username = message.from_user.username
-    varres = var_time + " " + username + "." + " Текущие дата и время -" + dt_string + ".\nВведите номер вашей группы, пожалуйста."
-    bot.send_message(message.from_user.id, varres)
+
+
+def get_text_messages(message):
+    if message.text == '/start':
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        data = dt_string.split(' ')[0]
+        time = dt_string.split(' ')[1]
+        if int(time.split(':')[0]) < 11:
+            var_time = "Доброе утро,"
+        elif int(time.split(':')[1]) >= 11 and int(time.split(':')[1])< 18:
+            var_time = "Добрый день,"
+        else:
+            var_time = "Добрый вечер,"
+        username = message.from_user.username
+        varres = var_time + " " + username + "." + " Текущие дата и время -" + dt_string + ".\nВведите номер вашей группы, пожалуйста."
+        bot.send_message(message.from_user.id, varres)
+    elif message.text == "abcd":
+        bot.send_message(message.from_user.id, "ABCD ABCD")
 bot.infinity_polling()
